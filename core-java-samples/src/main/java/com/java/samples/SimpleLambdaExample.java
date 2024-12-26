39b85ce33d2eb6b985e61b6720f3b302cc2ccabb
nagapavandev <tanguturi.pawan@gmail.com>
2024-12-26 21:21:20 +0530

    feat: updated Lambda examples

--- core-java-samples/src/main/java/com/java/samples/SimpleLambdaExample.java
+++ core-java-samples/src/main/java/com/java/samples/SimpleLambdaExample.java
@@ -6,6 +6,7 @@ public class SimpleLambdaExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         // Create a list of strings
+		/*Fixed length of arrayList will be returned*/
         List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
 
         // Using a Lambda expression to print each name in the list
@@ -13,10 +14,20 @@ public class SimpleLambdaExample {
         
         Integer [] agesArray = {40, 45, 35};
         
-        List<Integer> ages = Arrays.asList(agesArray);
+        List<Integer> ages = new ArrayList<>(Arrays.asList(agesArray));
+        
+        ages.add(55);
 
         // Using a Lambda expression to print each name in the list
         ages.forEach(age -> System.out.println(age));
+        Integer [] array = {1,4,5,10,9};
+        List<Integer> list = new ArrayList<>(array.length);
+        for (int num : array) {
+            list.add(num);  // Convert each primitive int to Integer
+        }
+        
+        list.add(55); 
+        System.out.println(list);
 
 	}
 
